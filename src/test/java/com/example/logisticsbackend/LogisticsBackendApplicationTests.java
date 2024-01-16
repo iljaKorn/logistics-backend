b7a8fa40dddaad9f080c2107d93801bc25c50ef9
iljaKorn <kornilov.ilja@rambler.ru>
2024-01-16 15:14:54 +0300

    Initial commit

---
+++ src/test/java/com/example/logisticsbackend/LogisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logisticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
